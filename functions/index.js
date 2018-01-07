2ff0a53b3e24eb3de53a9950718eca3d780b13fd
Nick Kasten <kastencode@gmail.com>
2018-01-06 19:49:46 -0600

    fixing folder structure

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,4 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.helloWorld = void 0;
-
-var functions = _interopRequireWildcard(require("firebase-functions"));
-
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
-
-var helloWorld = functions.https.onRequest(function (request, response) {
+import * as functions from 'firebase-functions';
+export const helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from ES6 Firebase!");
 });
-exports.helloWorld = helloWorld;
